669e0c7e19cbe218138ffd7359f3ec158b12316f
cypherlobo <badusb.pro@gmail.com>
2025-02-15 19:53:32 +0100

    Update setup-user.sh

--- scripts/setup-user.sh
+++ scripts/setup-user.sh
@@ -23,4 +23,4 @@ echo "$USERNAME:$PASSWORD" | chpasswd
 # Make sure to create user directories
 DEBIAN_FRONTEND=noninteractive apt-get install xdg-user-dirs
 
-sudo -u shields xdg-user-dirs-update
+sudo -u droidian xdg-user-dirs-update
